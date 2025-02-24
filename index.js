72ffe55693aeedf03ae9ba7622c6ea352e3056ab
emonShimoul <emonshimoul@gmail.com>
2025-02-25 01:50:06 +0600

    use of dotenv for db security

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
@@ -9,14 +10,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// coffeeMaster
-// 4UOazTv20kBx2fFz
-
-// const uri =
-// "mongodb+srv://<db_username>:<db_password>@cluster0.pabg0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-const uri =
-  "mongodb+srv://coffeeMaster:4UOazTv20kBx2fFz@cluster0.pabg0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pabg0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
