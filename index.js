f8cdc786a825f94a713b18df2b250758a6da4709
emonShimoul <emonshimoul@gmail.com>
2025-03-05 17:04:12 +0600

    remove unnecessary console

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pabg0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri);
+// console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -86,7 +86,7 @@ async function run() {
 
     app.post("/users", async (req, res) => {
       const newUser = req.body;
-      console.log("Creating new user", newUser);
+      // console.log("Creating new user", newUser);
       const result = await userCollection.insertOne(newUser);
       res.send(result);
     });
